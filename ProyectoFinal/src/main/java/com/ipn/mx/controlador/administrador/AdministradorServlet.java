9d20216c1855fbd93bf0efc054def798e26abc27
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2022-01-02 22:50:15 -0600

    crud administrador

---
+++ ProyectoFinal/src/main/java/com/ipn/mx/controlador/administrador/AdministradorServlet.java
@@ -0,0 +1,218 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.ipn.mx.controlador.administrador;
+
+import com.ipn.mx.modelo.dao.AdministradorDAO;
+import com.ipn.mx.modelo.dao.UsuarioDAO;
+import com.ipn.mx.modelo.dto.AdministradorDTO;
+import com.ipn.mx.modelo.dto.UsuarioDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author calebbolanos
+ */
+@WebServlet(name = "AdministradorServlet", urlPatterns = {"/administrador/AdministradorServlet"})
+public class AdministradorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession sesion = request.getSession();
+        if (sesion.getAttribute("dtoAdministrador") == null) {
+            response.sendRedirect("iniciarSesion.jsp");
+            return;
+        }
+
+        String accion = request.getParameter("accion") == null ? "" : request.getParameter("accion");
+
+        switch (accion) {
+            case "listaDeAdministradores":
+                listaDeAdministradores(request, response);
+                break;
+            case "nuevo":
+                agregarAdministrador(request, response);
+                break;
+            case "guardar":
+                almacenarAdministrador(request, response);
+                break;
+            case "actualizar":
+                actualizarAdministrador(request, response);
+                break;
+            case "eliminar":
+                eliminarAdministrador(request, response);
+                break;
+            case "ver":
+                mostrarAdministrador(request, response);
+                break;
+            case "verReporte":
+                //mostrarReporte(request, response);
+                break;
+            case "graficar":
+                //mostrarGrafica(request, response);
+                break;
+            default:
+                listaDeAdministradores(request, response);
+                break;
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private void listaDeAdministradores(HttpServletRequest request, HttpServletResponse response) {
+        AdministradorDAO dao = new AdministradorDAO();
+        try {
+            Collection lista = dao.readAll();
+            request.setAttribute("listaDeAdministradores", lista);
+
+            RequestDispatcher rd = request.getRequestDispatcher("listaAdministradores.jsp");
+            rd.forward(request, response);
+        } catch (SQLException | ServletException | IOException ex) {
+            Logger.getLogger(ClienteServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void agregarAdministrador(HttpServletRequest request, HttpServletResponse response) {
+        RequestDispatcher vista = request.getRequestDispatcher("administradorForm.jsp");
+        try {
+            vista.forward(request, response);
+        } catch (ServletException | IOException ex) {
+            Logger.getLogger(ClienteServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void almacenarAdministrador(HttpServletRequest request, HttpServletResponse response) {
+        AdministradorDAO dao = new AdministradorDAO();
+        AdministradorDTO dto = new AdministradorDTO();
+        
+        if(!request.getParameter("txtIdAdministrador").equals(""))
+            dto.getEntidad().setIdAdministrador(Integer.parseInt(request.getParameter("txtIdAdministrador")));
+        
+        dto.getEntidad().setNombre(request.getParameter("txtNombre"));
+        dto.getEntidad().setUsuario(request.getParameter("txtUsuario"));
+        dto.getEntidad().setContrasena(request.getParameter("txtContrasena"));
+        
+        try {
+            
+            if(!request.getParameter("txtIdAdministrador").equals("")){//CREAR
+                dao.update(dto);            
+                request.setAttribute("mensaje", "Administrador actualizado con exito.");
+            }else{
+                dao.create(dto);            
+                request.setAttribute("mensaje", "Administrador agregado con exito.");
+            }            
+            
+            listaDeAdministradores(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProveedorServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void actualizarAdministrador(HttpServletRequest request, HttpServletResponse response) {
+        AdministradorDAO dao = new AdministradorDAO();
+        AdministradorDTO dto = new AdministradorDTO();
+        dto.getEntidad().setIdAdministrador(Integer.parseInt(request.getParameter("id")));
+        
+        RequestDispatcher vista = request.getRequestDispatcher("administradorForm.jsp");
+
+        try {
+            dto = dao.read(dto);
+            request.setAttribute("administrador", dto);
+            vista.forward(request, response);
+
+        } catch (SQLException | ServletException | IOException ex) {
+            Logger.getLogger(ProductoServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void eliminarAdministrador(HttpServletRequest request, HttpServletResponse response) {
+        AdministradorDAO dao = new AdministradorDAO();
+        AdministradorDTO dto = new AdministradorDTO();
+        dto.getEntidad().setIdAdministrador(Integer.parseInt(request.getParameter("id")));
+        
+        try {
+            dao.delete(dto);
+            request.setAttribute("mensaje", "Cliente eliminado con exito.");
+            listaDeAdministradores(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProveedorServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void mostrarAdministrador(HttpServletRequest request, HttpServletResponse response) {
+        AdministradorDAO dao = new AdministradorDAO();
+        AdministradorDTO dto = new AdministradorDTO();
+        dto.getEntidad().setIdAdministrador(Integer.parseInt(request.getParameter("id")));
+        RequestDispatcher vista = request.getRequestDispatcher("datosAdministrador.jsp");
+
+        try {
+            dto = dao.read(dto);
+            request.setAttribute("administrador", dto);
+            vista.forward(request, response);
+
+        } catch (SQLException | ServletException | IOException ex) {
+            Logger.getLogger(ProveedorServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
---
+++ ProyectoFinal/src/main/webapp/administrador/datosAdministrador.jsp
@@ -0,0 +1,144 @@
+<%-- 
+    Document   : datosAdministrador
+    Created on : 2 ene. 2022, 22:41:19
+    Author     : calebbolanos
+--%>
+
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page import="com.ipn.mx.modelo.dto.AdministradorDTO"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%
+    HttpSession sesion = request.getSession();
+    if (sesion.getAttribute("dtoAdministrador") == null) {
+        response.sendRedirect("iniciarSesion.jsp");
+        return;
+    }
+    AdministradorDTO dtoAdmin = (AdministradorDTO) sesion.getAttribute("dtoAdministrador");
+%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js" ></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+
+        <title>Administrador - Clientes</title>
+    </head>
+    <body>
+        <div id="app">
+            <v-app>
+                <v-app-bar app style=" background-color: rgba(255, 255, 255, 0.5) !important; backdrop-filter: saturate(180%) blur(20px) !important;">
+
+                    <v-app-bar-title>ElectrodoShop</v-app-bar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn color="secondary" text rounded href="inicio.jsp">Inicio</v-btn>
+                    <v-btn color="secondary" text rounded href="CategoriaServlet?accion=listaDeCategorias">Categorías</v-btn>
+                    <v-btn color="secondary" text rounded href="ProveedorServlet?accion=listaDeProveedores">Proveedores</v-btn>
+                    <v-btn color="secondary" text rounded href="ProductoServlet?accion=listaDeProductos">Productos</v-btn>
+                    <v-btn color="secondary" text rounded href="ClienteServlet?accion=listaDeClientes">Clientes</v-btn>
+                    <v-btn color="primary" text rounded href="AdministradorServlet?accion=listaDeAdministradores">Administradores</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-menu left bottom offset-y rounded="lg">
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn icon v-bind="attrs" v-on="on">
+                                <v-avatar color="accent darken-1 shrink" size="32">
+                                    <v-img
+                                        alt=""
+                                        :src="imagenUsuario"
+                                        ></v-img>
+                                </v-avatar>
+                            </v-btn>
+                        </template>
+
+                        <v-list>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title class="text-h6">
+                                        {{nombreUsuario}}
+                                    </v-list-item-title>
+                                    <v-list-item-subtitle>
+                                        {{correo}}
+                                    </v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-list-item>
+
+                            <v-divider></v-divider>
+                            <v-list-item href="AdministradorServlet?accion=listaDeAdministradores">
+                                <v-list-item-title>Configuración de Administradores</v-list-item-title>
+                            </v-list-item>
+                            <v-list-item href="../CerrarSesion">
+                                <v-list-item-title>Cerrar Sesión</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+
+                </v-app-bar>
+
+                <v-main class="grey lighten-2">
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12" class="">
+                                <h2 class="mt-2">Datos del Cliente</h2>
+                            </v-col>
+                            <v-col cols="12" class="">
+                                <v-card class="pa-3 ">
+                                    <v-card-text>
+                                            <v-row>
+                                                <v-col cols="12" class="mt-2">
+                                                    <v-text-field readonly v-model="idAdministrador" name="txtIdAdministrador" label="ID de administrador" type="number" outlined ></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12">
+                                                    <v-text-field readonly v-model="nombre" name="txtNombre" label="Nombre" type="text" outlined  required :rules="reglas"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12">
+                                                    <v-text-field readonly v-model="usuario" name="txtUsuario" label="Usuario" type="text" outlined  required :rules="reglas"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12">
+                                                    <v-text-field readonly v-model="contrasena" name="txtContrasena" label="Contraseña" type="password" outlined  required :rules="reglas"></v-text-field>
+                                                </v-col>
+                                            </v-row>
+                                    </v-card-text>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+
+                    </v-container>
+                </v-main>
+
+            </v-app>
+
+        </div>
+
+        <script>
+new Vue({
+    el: '#app',
+    data: () => ({
+            drawer: false,
+            group: 0,
+            nombreUsuario: '<%=dtoAdmin.getEntidad().getNombre()%>',
+            correo: '<%=dtoAdmin.getEntidad().getUsuario()%>',
+            imagenUsuario: 'https://themeselection.com/demo/materio-vuetify-vuejs-admin-template-free/demo/img/1.e2938115.png',
+
+            idAdministrador: '<c:out value="${administrador.entidad.idAdministrador}"/>',
+            nombre: '<c:out value="${administrador.entidad.nombre}"/>',
+            usuario: '<c:out value="${administrador.entidad.usuario}"/>',
+            contrasena: '<c:out value="${administrador.entidad.contrasena}"/>',
+            reglas: [
+                v => !!v || "Este campo es requerido",
+            ],
+            reglasEdad: [
+                v => !!v || "Este campo es requerido",
+                v => (v && v >= 18) || "Debes de tener al menos 18 años",
+            ],
+        }),
+    vuetify: new Vuetify(),
+})
+        </script>
+    </body>
+</html>
